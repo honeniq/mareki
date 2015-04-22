f091707b93eaa6592489819c9bf6f3d1059b56ee
honeniq <honeniq@gmail.com>
2015-04-22 22:25:25 +0900

    Implement mareki_eto

--- lib/mareki.rb
+++ lib/mareki.rb
@@ -8,4 +8,9 @@ class Date
       self.year - 1999
     end
   end
+
+  def mareki_eto
+    eto = ['河童', 'つちのこ', '三ツ首竜', '鵺', '一角獣', 'モグラザメ', '化猫', '蝦蟇', '火の鳥', '怪奇植物', '人魚', '夜叉', '妖精']
+    eto[(self.year % 13) - 1]
+  end
 end
--- spec/mareki/mareki_spec.rb
+++ spec/mareki/mareki_spec.rb
@@ -3,7 +3,7 @@ require "date"
 
 describe Date do
   describe "#mareki_year" do
-    it "符号付き整数を返す" do
+    it "Fixnumを返す" do
       expect(Date.new(2015, 1, 1).mareki_year).to be_a(Fixnum)
     end
 
@@ -17,6 +17,24 @@ describe Date do
 
     it "西暦2015年は魔暦17年" do
       expect(Date.new(2015, 1, 1).mareki_year).to eq 17 
+    end
+  end
+
+  describe "#mareki_eto" do
+    it "Stringを返す"do
+      expect(Date.new(2015, 1, 1).mareki_eto).to be_a(String)
+    end
+
+    it "1985年の悪魔干支は火の鳥" do
+      expect(Date.new(1985, 1, 1).mareki_eto).to eq("火の鳥")
+    end
+
+    it "1999年の悪魔干支は怪奇植物" do
+      expect(Date.new(1999, 1, 1).mareki_eto).to eq("怪奇植物")
+    end
+
+    it "2015年の悪魔干支は妖精" do
+      expect(Date.new(2015, 1, 1).mareki_eto).to eq("妖精")
     end
   end
 end
